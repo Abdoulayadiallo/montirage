59be43d5635d8e2ec4956a673fb99c6f197e8bda
nurdev1 <fatoudevweb1@gmail.com>
2022-09-08 15:16:44 +0000

    Premier component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonTirageComponent } from './mon-tirage/mon-tirage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonTirageComponent
   ],
   imports: [
     BrowserModule,
