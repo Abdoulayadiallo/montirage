2323754cf74418c8d8974667daace322f0a86394
nurdev1 <fatoudevweb1@gmail.com>
2022-09-08 15:55:43 +0000

    mis a jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MonTirageComponent } from './mon-tirage/mon-tirage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MonTirageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
