2eac8486685c1062fe0cc8ab10e27b90ca2b9dde
nurdev1 <fatoudevweb1@gmail.com>
2022-09-08 16:34:46 +0000

    header component

--- src/app/mon-tirage/mon-tirage.component.ts
+++ src/app/mon-tirage/mon-tirage.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-mon-tirage',
   templateUrl: './mon-tirage.component.html',
   styleUrls: ['./mon-tirage.component.css']
 })
-export class MonTirageComponent implements OnInit {
+export class MonTirageComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ 
 
 }
